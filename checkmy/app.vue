f554da6082621ddd3f0ad14d6640ea3237eb724e
Adi Wira Pratama <adiwira85pratama@gmail.com>
2024-07-13 02:41:56 +0700

    final commit before deployment

--- checkmy/app.vue
+++ checkmy/app.vue
@@ -1,15 +1,34 @@
 <script setup lang="ts">
+const inputValidation:any = ref(null);
+const inputImage:any = ref(null);
+const results:any = ref([]);
+
+/*
+    Current hardcoded implmentation of a validation database
+*/
+const templateValidation:any = {
+    contract_xs:'What is the overall purpose and scope of the contract?\nWho are the parties involved, and what are their roles?\nAre all terms and definitions clear and unambiguous?',
+    contract_md:'What is the overall purpose and scope of the contract?\nWho are the parties involved, and what are their roles?\nAre all terms and definitions clear and unambiguous?\nWhat are the obligations and responsibilities of each party?\nWhat products, services, or outcomes are expected?\nWhat is the payment schedule, including amounts and methods?', 
+    contract_xl:'What is the overall purpose and scope of the contract?\nWho are the parties involved, and what are their roles?\nAre all terms and definitions clear and unambiguous?\nWhat are the obligations and responsibilities of each party?\nWhat products, services, or outcomes are expected?\nWhat is the payment schedule, including amounts and methods?\nWhat is the governing law and jurisdiction for the contract?\nAre all regulatory requirements being complied with?\nAre confidentiality and non-disclosure clauses clearly defined?\nHow is the handling and ownership of intellectual property addressed?\nWhat are the liability and indemnity provisions?\nWhat are the terms and conditions for contract termination?\nAre there provisions for unforeseen circumstances (force majeure)?\nWhat is the process for resolving disputes?\nAre there clauses related to arbitration or mediation?\nWhat is the effective date of the contract?\nWhat is the contract duration, and are there any renewal terms?\nWhat are the deadlines and timelines for deliverables and milestones?\nAre the signatures from authorized representatives of all parties included?\nAre witness signatures required, and if so, are they included?\nAre all referenced exhibits, appendices, and schedules included and complete?\nAre there any amendments or changes to the original contract?\nHow are risks allocated between the parties?\nAre insurance requirements and coverage confirmed?\nIs there consistency throughout the contract with no conflicting terms?\nHave all typographical or grammatical errors been checked?\nIs there a need for legal advice to clarify any uncertainties?',
+}
+const pickedTemplateValidation:any = ref(null);
+
+/*
+    Model query handler functions
+*/
+const running:any = ref(false);
 const getBase64 = (file:any, callback:any) => {
     const reader:any = new FileReader()
     reader.readAsDataURL(file);
     reader.onload = () => {
         const base64Image = reader.result.replace(/^data:image\/(png|jpg|jpeg);base64,/, '');
+        inputImage.value = reader.result
         callback(base64Image);
     };
     reader.onerror = (error:any) => console.log('Error: ', error);
 }
 
-const query = (payload:any, callback:any) => {
+const query = async (payload:any, callback:any) => {
     const imageFile = payload.inputs.image;
 
     getBase64(imageFile, async (base64Image:string) => {
@@ -18,7 +37,7 @@ const query = (payload:any, callback:any) => {
             "https://api-inference.huggingface.co/models/naver-clova-ix/donut-base-finetuned-docvqa",
             {
                 headers: {
-                    Authorization: "Bearer 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",
+                    Authorization: "Bearer <INSERT HF TOKEN HERE>",
                     "Content-Type": "application/json",
                 },
                 method: "POST",
@@ -26,7 +45,6 @@ const query = (payload:any, callback:any) => {
             }
         );
         const result = await response.json();
-        console.log(result)
         callback(result)
     });
 }
@@ -37,8 +55,11 @@ const fileOnChange = async (e:any) => {
         return;
     }
 
-    // const templateItems = evaluationTemplate.value.split("\n")
-    await templateItems.forEach(async (item:any) => {
+    const validationItems = inputValidation.value.split("\n");
+    results.value = [];
+    running.value = true;
+
+    await validationItems.forEach(async (item:any) => {
         const payload = {
             inputs: {
                 image: file,
@@ -46,61 +67,87 @@ const fileOnChange = async (e:any) => {
             }
         };
         await query(payload, (result:any) => {
-            console.log(result[0].answer)
+            results.value.push({
+                question: payload.inputs.question,
+                answer: result[0].answer
+            })
         });
     })
+    running.value = false;
 };
 
 </script>
 
 <template>
-    <main class="min-h-screen flex flex-col items-center w-full p-20">
-        <div class="flex flex-col items-center w-2/3 space-y-8">
-            <h1 
-                class="text-6xl text-center leading-xl"
-            >
-                Leverage the power of AI to avoid legal troubles!
-            </h1>
-            <div id="analysis-result"></div>
-
-            <p>
-                Pick an evaluation template or create your own:
-            </p>
-            <textarea 
-                id="evaluation-template"
-                class="border-2 w-full"
-                placeholder="
-                    Checklist for a general contract:
-                    What is the overall purpose and scope of the contract?
-                    Who are the parties involved, and what are their roles?
-                    What are the obligations and responsibilities of each party?
-                    What products, services, or outcomes are expected?
-                "></textarea>
-            <p id="status"></p>
-            
-        </div>
-
-        <div 
-            class="
-                w-1/2 h-[60vh]
-                flex flex-col items-center justify-center
-                bg-white border rounded-lg shadow-lg
-            "
+    <main 
+        style="background-image: url('gradient.jpeg')"
+        class="min-h-screen flex flex-col items-center justify-center w-full p-20"
+    >
+        <h1 
+            class="font-primary text-5xl text-center leading-xl pb-4 font-bold"
         >
-            <div class="h-40[vh]" id="image-container object-contain"></div>
-            <label class="custom-file-upload flex flex-col items-center">
+            Leverage the power of AI to keep you legally safe!
+        </h1>
+        <p class="font-primary pb-10">Currently, checkmy.xyz uses the huggingface inference API to perform the analysis. The implementation for fully private use of llm is incoming!</p>
+        <div class="flex flex-row space-x-2">
+            <div class="flex flex-col items-center space-y-2 bg-white p-8 rounded-xl border-b-4 shadow-xl font-primary">
+                <h2 class="mt-2 text-2xl">Pick an Evaluation Template or Create Your Own</h2>
+                <div class="flex flex-row space-x-2">
+                    <button 
+                        v-for="templateName, idx in Object.keys(templateValidation)"
+                        @click="inputValidation = templateValidation[templateName]; pickedTemplateValidation = templateName"
+                        :class="pickedTemplateValidation == templateName ? 'btn-primary': 'btn-outline'"
+                        class="btn btn-xs font-thin"
+                    >
+                        {{ templateName }}
+                    </button>
+                </div>
+                <textarea 
+                    v-model="inputValidation"
+                    id="evaluation-template"
+                    class="border-2 w-full h-full rounded-lg"
+                >
+                </textarea>
+                <p v-if="pickedTemplateValidation == 'contract_xl'" class="text-error text-xs">
+                    warning: since we are calling through the huggingface API, this might not work
+                </p>
+            </div>
+            <div 
+                class="flex flex-col items-center space-y-2 bg-white p-8 rounded-xl border-b-4 shadow-xl font-primary"
+                v-if="inputValidation"
+            >
+                <h2 class="mt-2 text-2xl">Upload Your Legal Documents</h2>
                 <img 
-                    class="upload-icon h-[20vh] object-contain" 
-                    src="https://huggingface.co/datasets/Xenova/transformers.js-docs/resolve/main/upload-icon.png"
+                    v-if="inputImage"
+                    class="upload-icon h-[30vh] object-contain" 
+                    :src="inputImage"
                 />
-                <input
-                    class="border"
-                    id="file-upload" 
-                    type="file" 
-                    accept="image/*" 
-                />
-            </label>
-            <h2 class="mt-2 text-2xl">Upload Your Legal Documents</h2>
+                <label class="custom-file-upload flex flex-col items-center">
+                    <img 
+                        class="upload-icon h-[10vh] object-contain" 
+                        src="https://huggingface.co/datasets/Xenova/transformers.js-docs/resolve/main/upload-icon.png"
+                    />
+                    <input
+                        class="border"
+                        id="file-upload" 
+                        type="file" 
+                        accept="image/*" 
+                        @change="fileOnChange"
+                    />
+                </label>
+            </div>
         </div>
+        <div 
+            v-if="results.length > 0 && !running"
+            class="w-2/3  mt-4 flex flex-col items-center space-y-2 bg-white p-8 rounded-xl border-b-4 shadow-xl font-primary"
+        >
+            <h2 class="mt-2 text-2xl">Analyze Result</h2>
+            <p
+                v-for="result, idx in results"
+                :key="`${result}_${idx}`"
+            > {{ result.question }} -> {{ result.answer }}</p>
+        </div>
+        <div v-if="running">Analyzing...</div>
+        
     </main>
 </template>
